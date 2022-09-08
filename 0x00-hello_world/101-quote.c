083e99f7cf1c0feb3caadaaa441aa8b22575cfdf
abrshkorra <abrshkorra@gmail.com>
2022-09-08 08:14:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<unistd.h>
+
 /**
  * main - Entry point
  *
@@ -11,7 +12,7 @@ int main(void)
 {
 	
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+
 	write(1, quo, 59);
 	return (1);
 }
