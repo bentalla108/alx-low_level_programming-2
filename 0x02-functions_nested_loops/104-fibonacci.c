a5cec9990a4b20225ae690df8b241f0f833d0ca8
abrshkorra <abrshkorra@gmail.com>
2022-09-13 16:58:17 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * main - finds and prints the first 98 Fibonacci numbers, 
+ * starting with 1 and 2
  * followed by a new line
  * Return: 0
  */
