01411d3b4e0a2aed4cd40eb816eebb1d5e2c7b79
abrshkorra <abrshkorra@gmail.com>
2022-09-13 13:34:05 -0700

    2nd task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * print_alphabet prints alphabet of letters
+ *
+ * Returns void
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
