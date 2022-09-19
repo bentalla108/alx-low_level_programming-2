6059d2fbdabc09be9b33e4763f820b0060ea06f3
abrshkorra <abrshkorra@gmail.com>
2022-09-19 15:01:01 -0700

    1st commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates a integer value to 98
+ *
+ * @x: A pointer to an int that will be updated
+ *
+ * Return: void that means is correct
+ */
+
+void reset_to_98(int *n)
+{
+	*x = 98;
+}
