640dd70edd3f9a4f28de609898b9c1369d602cdd
Abrham Abayneh Kora <abrshkorra@gmail.com>
2022-09-23 02:16:03 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -21,24 +21,17 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + a) != '\0')
 		a++;
-
 	while (*(n2 + b) != '\0')
 		b++;
-
 	if (a >= b)
 		g = a;
 	else
 		g = b;
-
 	if (size_r <= g + 1)
 		return (0);
 	r[g + 1] = '\0';
-	a--;
-	b--;
-	size_r--;
-	c = *(n1 + a) - 48;
-	d = *(n2 + b) - 48;
-
+	a--, b--, size_r--;
+	c = *(n1 + a) - 48, d = *(n2 + b) - 48;
 	while (g >= 0)
 	{
 		o = c + d + add;
@@ -46,32 +39,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			add = o / 10;
 		else
 			add = 0;
-
 		if (o > 0)
 			*(r + g) = (o % 10) + 48;
 		else
 			*(r + g) = '0';
-
 		if (a > 0)
-		{
-			a--;
-			c = *(n1 + a) - 48;
-		}
+			a--, c = *(n1 + a) - 48;
 		else
 			c = 0;
-
 		if (b > 0)
-		{
-			b--;
-			d = *(n2 + b) - 48;
-		}
+			b--, d = *(n2 + b) - 48;
 		else
 			d = 0;
-
-		g--;
-		size_r--;
+		g--, size_r--;
 	}
-
 	if (*(r) == '0')
 		return (r + 1);
 	else
