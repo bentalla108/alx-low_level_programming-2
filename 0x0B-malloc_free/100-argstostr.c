d600875e079d56bbdb81fc2395d82bb48b29293c
Abrham Abayneh Kora <abrshkorra@gmail.com>
2022-10-03 12:07:52 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,44 +2,51 @@
 #include <stdlib.h>
 
 /**
- * argstostr - prints args
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
  *
- * @ac: takes in width of grid
- *
- * @av: height of grid
- *
- * Return: the args one line at a time
+ * Return: pointer of an array of char
  */
-
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int t = 0, a = 0, b = 0, c = 0;
+	char *aout;
+	int c, i, j, ia;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0)
 		return (NULL);
-	while (a < ac)
+
+	for (c = i = 0; i < ac; i++)
 	{
-		while (av[a][b] != '\0')
-		{
-			t++;
-			b++;
-		}
-		a++;
-	}
-	t = t + ac + 1;
-	str = malloc(sizeof(char) * t);
-	if (str == NULL)
-		return (NULL);
-	for (a = 0; a < ac; a++)
-	{
-		for (b = 0; av[a][b] != '\0'; b++)
-		{
-			str[c] = av[a][b];
+		if (av[i] == NULL)
+			return (NULL);
+
+		for (j = 0; av[i][j] != '\0'; j++)
 			c++;
-		}
-		str[c] = '\n';
 		c++;
 	}
-	return (str);
+
+	aout = malloc((c + 1) * sizeof(char));
+
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
+
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+	aout[ia] = '\0';
+
+	return (aout);
 }
