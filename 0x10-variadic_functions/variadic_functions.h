8e2c216289172bd051c9ac942709ad47f245bce3
abrsh <abrshkorra@gmail.com>
2022-10-13 18:05:28 +0300

    edit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,10 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
 
+typedef struct print
+{
+    char *symbol;
+    void (*print_func)(va_list arg);
+} func_printer;
+
 #endif
